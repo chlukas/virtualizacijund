f3326557cb209679f61ac051265257bc10a2d2fe
Arnas Fomenko <arnas.fomenko@gmail.com>
2017-12-21 09:09:59 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 $source = file_get_contents("dbIP.txt");
-$link = mysqli_connect($source, "root", "root", "db_name");
+$link = mysqli_connect($source, "root", "root", "virtualizacijos");
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
